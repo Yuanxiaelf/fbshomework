c966b3e4d7bbcac88b9b4546996533fec61c62ed
Yuanxiaelf <1056606933@qq.com>
2026-03-30 15:30:24 +0800

    init: 初始化Spring Boot项目

---
+++ src/main/java/com/fbshomework/fbshomework/FbshomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.fbshomework.fbshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbshomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbshomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbshomework/fbshomework/FbshomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbshomework.fbshomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbshomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
